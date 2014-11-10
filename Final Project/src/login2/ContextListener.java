25cbca5ad0d2e648fb9ffdb251aa42b1cae49ff0
Dennis Wang <dwang22@stanford.edu>
2014-11-10 15:32:32 -0800

    Trying proj5 again

---
+++ Final Project/src/login2/ContextListener.java
@@ -0,0 +1,41 @@
+package login2;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ContextListener
+ *
+ */
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ContextListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+    	AccountManager am = new AccountManager();
+    	am.createAccount("Patrick", "1234");
+    	am.createAccount("Molly", "FloPup");
+    	
+        ServletContext sc = arg0.getServletContext();
+        sc.setAttribute("accountManager", am);
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
